1753b45307ea1e5b4b8dd1f9656fc7c300cc6ec5
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-21 18:43:06 -0300

    fix: product table not created properly

--- hooks/useDB.ts
+++ hooks/useDB.ts
@@ -5,17 +5,15 @@ export default function useDB() {
   const db = SQLite.openDatabase('shop.db');
 
   const initializeDB = useCallback(async () => {
-    const sql = `
-      CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        price REAL NOT NULL,
-        quantity INTEGER NOT NULL,
-        image TEXT NOT NULL,
-        description TEXT NOT NULL,
-        category TEXT NOT NULL,
-      )
-    `;
+    const sql = `CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      price REAL NOT NULL,
+      quantity INTEGER NOT NULL,
+      image TEXT NOT NULL,
+      description TEXT NOT NULL,
+      category TEXT NOT NULL
+    );`;
 
     await db.execAsync([{ sql, args: [] }], false);
     console.log('DB Initialized');
@@ -24,4 +22,6 @@ export default function useDB() {
   useEffect(() => {
     initializeDB();
   }, [initializeDB]);
+
+  return { db };
 }

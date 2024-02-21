c0b8105f93907b032c367e81fcddcff5a9a682b9
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-21 18:16:41 -0300

    feat: add create product form functionality

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,13 +1,177 @@
 import { Stack } from 'expo-router';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Controller, useForm } from 'react-hook-form';
+import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import tailshake from 'tailshake';
+
+import { Product } from '~/types';
 
 const ModalScreen = () => {
+  const { bottom } = useSafeAreaInsets();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<Product>({
+    mode: 'all',
+    defaultValues: { quantity: 1, price: 1, category: 'Clothing' },
+  });
+
+  const onFormSubmit = (payload: Product) => {
+    console.log({ payload });
+  };
+
   return (
-    <View>
+    <>
       <Stack.Screen options={{ title: 'Add new product' }} />
-      <Text>Modal</Text>
-    </View>
+      <View className="flex-1 p-5" style={{ paddingBottom: bottom }}>
+        <View className="flex-1">
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value}
+                className={tailshake(
+                  'bg-white p-5 border-2 rounded-full mb-5 placeholder:text-indigo-500 border-indigo-500 text-black',
+                  errors.name && 'text-red-800 border-red-800 placeholder:text-red-800'
+                )}
+                placeholder="Name"
+                onBlur={onBlur}
+                onChangeText={onChange}
+              />
+            )}
+            name="name"
+          />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value}
+                className={tailshake(
+                  'bg-white p-5 border-2 rounded-full mb-5 placeholder:text-indigo-500 border-indigo-500 text-black',
+                  errors.description && 'text-red-800 border-red-800 placeholder:text-red-800'
+                )}
+                placeholder="Description"
+                onBlur={onBlur}
+                onChangeText={onChange}
+              />
+            )}
+            name="description"
+          />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value}
+                className={tailshake(
+                  'bg-white p-5 border-2 rounded-full mb-5 placeholder:text-indigo-500 border-indigo-500 text-black',
+                  errors.category && 'text-red-800 border-red-800 placeholder:text-red-800'
+                )}
+                placeholder="Category"
+                onBlur={onBlur}
+                onChangeText={onChange}
+              />
+            )}
+            name="category"
+          />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+              min: 1,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value.toString()}
+                className={tailshake(
+                  'bg-white p-5 border-2 rounded-full mb-5 placeholder:text-indigo-500 border-indigo-500 text-black',
+                  errors.price && 'text-red-800 border-red-800 placeholder:text-red-800'
+                )}
+                placeholder="Price"
+                onBlur={onBlur}
+                onChangeText={(value) => {
+                  if (!value) {
+                    return onChange(0);
+                  }
+
+                  onChange(parseFloat(value));
+                }}
+              />
+            )}
+            name="price"
+          />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+              min: 1,
+              validate: (value) => !Number.isNaN(value),
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value.toString()}
+                className={tailshake(
+                  'bg-white p-5 border-2 rounded-full mb-5 placeholder:text-indigo-500 border-indigo-500 text-black',
+                  errors.quantity && 'text-red-800 border-red-800 placeholder:text-red-800'
+                )}
+                placeholder="Quantity"
+                onBlur={onBlur}
+                onChangeText={(value) => {
+                  if (!value) {
+                    return onChange(0);
+                  }
+
+                  onChange(parseInt(value, 10));
+                }}
+              />
+            )}
+            name="quantity"
+          />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value}
+                className={tailshake(
+                  'bg-white p-5 border-2 rounded-full mb-5 placeholder:text-indigo-500 border-indigo-500 text-black',
+                  errors.image && 'text-red-800 border-red-800 placeholder:text-red-800'
+                )}
+                placeholder="Image"
+                onBlur={onBlur}
+                onChangeText={onChange}
+              />
+            )}
+            name="image"
+          />
+        </View>
+
+        <TouchableOpacity
+          onPress={handleSubmit(onFormSubmit)}
+          disabled={!isValid}
+          className="items-center bg-indigo-500 rounded-[28px] shadow-md p-4 disabled:bg-zinc-400 disabled:shadow-none">
+          <Text className="text-white text-lg font-semibold text-center">Create</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export type Product = {
+  id?: number;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+  description: string;
+  category: string;
+};

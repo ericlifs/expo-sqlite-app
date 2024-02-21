1d37cd438896fc3bd3adcd16695986b49a2e3473
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-21 18:42:36 -0300

    feat: install react-query dependency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,22 @@
 import { Stack } from 'expo-router';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import useDB from '~/hooks/useDB';
 
 import '../global.css';
 
+const queryClient = new QueryClient();
+
 export default function Layout() {
   useDB();
 
   return (
-    <Stack screenOptions={{ headerBackTitleVisible: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="details/[id]" />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerBackTitleVisible: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="details/[id]" />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }

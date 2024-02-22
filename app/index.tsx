88dbe6e80ff170f99834796f734efbc4ba153144
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-22 09:30:49 -0300

    feat: add showing list of products

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,35 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Stack, Link } from 'expo-router';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Pressable, Text, View } from 'react-native';
 
 import useGetAllProducts from '~/queries/useGetAllProducts';
+import { Product } from '~/types';
+
+const formatPrice = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 
 export default function Page() {
   const { products } = useGetAllProducts();
 
-  console.log(products);
+  const renderProduct = ({ item }: { item: Product }) => (
+    <Link href={`/details/${item.id}`} asChild>
+      <Pressable className="bg-white rounded px-4 py-2 flex flex-row items-center">
+        <View className="flex-1 mr-5">
+          <Text className="font-bold">{item.name}</Text>
+          <Text className="mt-2">
+            {formatPrice.format(item.price)} x {item.category}
+          </Text>
+        </View>
+
+        <Ionicons name="chevron-forward" size={20} />
+      </Pressable>
+    </Link>
+  );
 
   return (
-    <View className={styles.container}>
+    <>
       <Stack.Screen
         options={{
           title: 'Products',
@@ -21,26 +40,14 @@ export default function Page() {
           ),
         }}
       />
-      <View className={styles.main}>
-        <View>
-          <Text className={styles.title}>Hello World</Text>
-          <Text className={styles.subtitle}>This is the first page of your app.</Text>
-        </View>
-        <Link href={{ pathname: `/details/[id]`, params: { id: 'Dan' } }} asChild>
-          <TouchableOpacity className={styles.button}>
-            <Text className={styles.buttonText}>Show Details</Text>
-          </TouchableOpacity>
-        </Link>
+      <View className="flex-1 p-5">
+        <FlatList
+          data={products}
+          keyExtractor={(item) => item.id!.toString()}
+          ItemSeparatorComponent={() => <View className="h-5" />}
+          renderItem={renderProduct}
+        />
       </View>
-    </View>
+    </>
   );
 }
-
-const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
-  buttonText: 'text-white text-lg font-semibold text-center',
-  container: 'flex-1 p-6',
-  main: 'flex-1 max-w-[960] justify-between',
-  title: 'text-[64px] font-bold',
-  subtitle: 'text-4xl text-gray-700',
-};
